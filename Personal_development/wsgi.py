0bbe19a3173f4e617f65218d94a9b5ad860ae6dd
yohei4 <yohei000421@gmail.com>
2021-08-14 21:44:14 +0900

    dockerfile追加

--- Personal_development/wsgi.py
+++ Personal_development/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Personal_development.settings')
+from django.core.wsgi import get_wsgi_application
+
+FILE_PATH = os.path.dirname(__file__)
+PROJECT_NAME = os.path.basename(FILE_PATH)
+
+sys.path.append(os.path.dirname(FILE_PATH))
+sys.path.append(FILE_PATH)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", PROJECT_NAME + ".settings")
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Personal_development.settings')
 
 application = get_wsgi_application()
